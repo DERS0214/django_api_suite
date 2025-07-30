b840c241c6b9bd0b2c2d33a67cc5dd6be119b76a
David Ramirez <scadrami@espol.edu.ec>
2025-07-30 14:54:37 +0000

    Guía 22

--- backend_data_server/settings.py
+++ backend_data_server/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "assets/"
+STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, STATIC_URL),
